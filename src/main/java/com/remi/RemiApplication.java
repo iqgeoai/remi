b4b61ee81b8301b5183da2eb770b0a0c7bff2d10
George Sand <iqgeo@yahoo.com>
2026-05-16 13:13:24 +0300

    feat: Spring Boot app entrypoint + datasource config

---
+++ src/main/java/com/remi/RemiApplication.java
@@ -0,0 +1,11 @@
+package com.remi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemiApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(RemiApplication.class, args);
+  }
+}
